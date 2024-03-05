d539560c7c7157cde686729996b39470ade5c2f1
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-05 19:58:20 +0800

    Add InfoController and properties profiles

---
+++ src/main/java/io/github/keeeper/learningspring/controller/InfoController.java
@@ -0,0 +1,27 @@
+package io.github.keeeper.learningspring.controller;
+
+import io.github.keeeper.learningspring.TaskConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class InfoController {
+
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("/info/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+    @GetMapping("/info/prop")
+    boolean myProp() {
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
+}
--- src/main/java/io/github/keeeper/learningspring/controller/TaskController.java
+++ src/main/java/io/github/keeeper/learningspring/controller/TaskController.java
@@ -2,13 +2,16 @@ package io.github.keeeper.learningspring.controller;
 
 import io.github.keeeper.learningspring.model.Task;
 import io.github.keeeper.learningspring.model.TaskRepository;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
@@ -29,13 +32,26 @@ public class TaskController {
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id){
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PutMapping("/tasks/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody Task toUpdate){
-        if (repository.existsById(id)){
+        if (!repository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
         toUpdate.setId(id);
         repository.save(toUpdate);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> saveTask(@RequestBody @Valid Task toCreate){
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 }
