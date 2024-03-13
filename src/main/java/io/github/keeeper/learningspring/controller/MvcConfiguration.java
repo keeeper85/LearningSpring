4e199870129417fb1163fdca0b3bbd1edbcd3402
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-13 12:13:53 +0800

    Add configurable set of interceptors

--- src/main/java/io/github/keeeper/learningspring/controller/MvcConfiguration.java
+++ src/main/java/io/github/keeeper/learningspring/controller/MvcConfiguration.java
@@ -1,14 +1,23 @@
 package io.github.keeeper.learningspring.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Set;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
+    private Set<HandlerInterceptor> interceptors;
+
+    public MvcConfiguration(Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+        interceptors.forEach(registry::addInterceptor);
     }
 }
