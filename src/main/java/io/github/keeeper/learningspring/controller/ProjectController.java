72c341f25e0580b35aa76a786696f8280ebe6591
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-14 16:22:23 +0800

    Add Model to project controller, update projects.html to show all possible project steps

--- src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
+++ src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package io.github.keeeper.learningspring.controller;
 
+import io.github.keeeper.learningspring.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/projects")
 public class ProjectController {
     @GetMapping
-    String showProjects(){
+    String showProjects(Model model){
+        model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
 }
