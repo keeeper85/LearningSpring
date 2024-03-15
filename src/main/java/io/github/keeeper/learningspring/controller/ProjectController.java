8781b98d813f8bb84fa11844af23e3708ca6a00f
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-15 11:45:16 +0800

    Add addProject to ProjectController

--- src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
+++ src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package io.github.keeeper.learningspring.controller;
 
 import io.github.keeeper.learningspring.model.ProjectStep;
+import io.github.keeeper.learningspring.model.logic.ProjectService;
 import io.github.keeeper.learningspring.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService service;
+
+    public ProjectController(ProjectService service) {
+        this.service = service;
+    }
+
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt!");
         return "projects";
     }
 
