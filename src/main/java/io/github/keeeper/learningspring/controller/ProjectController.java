d6119d53b312d7475ea688619f03c45d0d6fe1ed
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-15 11:30:08 +0800

    Add addProjectStep to ProjectController

--- src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
+++ src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package io.github.keeeper.learningspring.controller;
 
+import io.github.keeeper.learningspring.model.ProjectStep;
 import io.github.keeeper.learningspring.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +15,12 @@ public class ProjectController {
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
+        current.getSteps().add(new ProjectStep());
         return "projects";
     }
 }
