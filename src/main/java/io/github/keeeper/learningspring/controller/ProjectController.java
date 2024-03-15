5d6ec6d05e82e2ebb33af47b6809fcbbb0022337
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-03-15 13:07:19 +0800

    Add validation

--- src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
+++ src/main/java/io/github/keeeper/learningspring/controller/ProjectController.java
@@ -1,14 +1,19 @@
 package io.github.keeeper.learningspring.controller;
 
+import io.github.keeeper.learningspring.model.Project;
 import io.github.keeeper.learningspring.model.ProjectStep;
 import io.github.keeeper.learningspring.model.logic.ProjectService;
 import io.github.keeeper.learningspring.model.projection.ProjectWriteModel;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/projects")
@@ -25,17 +30,28 @@ public class ProjectController {
         return "projects";
     }
 
-    @PostMapping(params = "addStep")
-    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+    @PostMapping
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel current,
+            BindingResult bindingResult,
+            Model model
+    ){
+        if (bindingResult.hasErrors()) return "projects";
+
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano projekt!");
         return "projects";
     }
 
-    @PostMapping
+    @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
         current.getSteps().add(new ProjectStep());
         return "projects";
+    }
+
+    @ModelAttribute("projects")
+    List<Project> getProjects(){
+        return service.readAll();
     }
 }
